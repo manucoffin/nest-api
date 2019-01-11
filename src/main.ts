ec3a59e1eb520ebb1a8b6fb78a0137b5c4274de5
manucoffin <manuel.coffin@gmail.com>
2019-01-11 10:43:12 +0100

    Added TSLint config and swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+  .setTitle('Blog API')
+  .setDescription('Blog API Documentation')
+  .setVersion('1.0')
+  .addTag('docs')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(configService.getNumber('PORT'));
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param } from '@nestjs/common';
+import { ApiResponse, ApiUseTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
+@ApiUseTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User trouvé et retourné',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User non trouvé :/',
+  })
   async getById(@Param('id') id: string) {
     return this.userService.getById(id);
   }
