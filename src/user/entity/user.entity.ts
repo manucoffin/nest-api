45d171962fd980c06463089959ef0664e75e03ac
manucoffin <manuel.coffin@gmail.com>
2019-01-11 15:17:39 +0100

    refactor(User): Added avatar and category columns to user entity
    fix #52

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -5,18 +5,24 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { UserCategory } from '../enums/user-category.enum';
 
 @Entity()
 export class User {
-
   @CreateDateColumn()
   created: Date;
 
   @Column({ type: 'varchar', name: 'email', length: 200 })
   email: string;
 
+  @Column({ type: 'bytea', name: 'file' })
+  file: ArrayBuffer;
+
   @Column({ type: 'varchar', name: 'first_name', length: 100 })
   firstName: string;
+
+  @PrimaryGeneratedColumn('uuid', { name: 'user_id' })
+  id: string;
 
   @Column({ type: 'varchar', name: 'last_name', length: 100 })
   lastName: string;
@@ -27,8 +33,14 @@ export class User {
   @Column({ type: 'varchar', name: 'password' })
   password: string;
 
+  @Column({
+    type: 'enum',
+    name: 'type',
+    enum: Object.keys(UserCategory).map(item => UserCategory[item]),
+    default: 'Standard',
+  })
+  type: UserCategory;
+
   @UpdateDateColumn()
   updated: Date;
-  @PrimaryGeneratedColumn('uuid', { name: 'user_id' })
-  userId: string;
 }
